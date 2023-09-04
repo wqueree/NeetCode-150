28db71b99a249d6d765faa12bb2f06dbacd9fbf6
Will Queree <wqueree@gmail.com>
2023-09-04 18:30:46 +0100

    Added C++ solution to 217

---
+++ arrays-hashing/217_contains_duplicate.cpp
@@ -0,0 +1,16 @@
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for (int& i : nums) {
+            if (seen.find(i) != seen.end()) {
+                return true;
+            }
+            seen.insert(i);
+        }
+        return false;
+    }
+};
