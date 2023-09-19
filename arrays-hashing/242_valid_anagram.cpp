0da7884ff5334d0c742ebf1b8f450edabddbec04
Will Queree <wqueree@gmail.com>
2023-09-19 22:01:58 +0100

    Updated 242

--- arrays-hashing/242_valid_anagram.cpp
+++ arrays-hashing/242_valid_anagram.cpp
@@ -6,10 +6,10 @@ public:
     bool isAnagram(std::string s, std::string t) {
         std::array<int, 26> s_counts = {0};
         std::array<int, 26> t_counts = {0};
-        for (char c : s) {
+        for (char& c : s) {
             s_counts[c - 'a']++;
         }
-        for (char c : t) {
+        for (char& c : t) {
             t_counts[c - 'a']++;
         }
         return s_counts == t_counts;
