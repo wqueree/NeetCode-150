c053a488cf3b6db73efa8a4da9d05fac0b3e72d1
Will Queree <wqueree@gmail.com>
2023-09-30 21:56:38 +0100

    A small indexing amendment to 746

--- 1d-dynamic-programming/746_min_cost_climbing_stairs.py
+++ 1d-dynamic-programming/746_min_cost_climbing_stairs.py
@@ -8,4 +8,4 @@ class Solution:
             return min(cost[0], cost[1])
         for i in range(2, n + 1):
             dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
-        return dp[-1]
+        return dp[n]
