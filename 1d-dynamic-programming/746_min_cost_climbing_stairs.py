861a93c452e895c3644b4c39fc3d7c1fb6a2a84a
Will Queree <wqueree@gmail.com>
2023-09-30 22:00:53 +0100

    Cleaner solution to 746 using reverse indexing

--- 1d-dynamic-programming/746_min_cost_climbing_stairs.py
+++ 1d-dynamic-programming/746_min_cost_climbing_stairs.py
@@ -2,10 +2,6 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n: int = len(cost)
-        dp: List[int] = [0 for _ in range(n + 1)]
-        if n == 2:
-            return min(cost[0], cost[1])
-        for i in range(2, n + 1):
-            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
-        return dp[n]
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
