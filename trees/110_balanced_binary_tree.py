50d751dd489a9e0c6d6b9b62bdd59697db7ec610
Will Queree <wqueree@gmail.com>
2023-10-26 20:57:00 +0100

    Added 110

---
+++ trees/110_balanced_binary_tree.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def depth(self, root: Optional[TreeNode]) -> int:
+        return 1 + max(self.depth(root.left), self.depth(root.right)) if root else 0
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        if abs(self.depth(root.left) - self.depth(root.right)) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
