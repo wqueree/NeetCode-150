e65cbfa45eef47d4eca52bcf39c04b3a63f173a5
Will Queree <wqueree@gmail.com>
2023-12-01 14:23:56 +0000

    Added 210

---
+++ graphs/210_course_schedule_ii.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from typing import Dict, List, Set
+
+
+class Solution:
+
+    
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegrees: Dict[int, int] = defaultdict(int)
+        adjacents: Dict[int, Set[int]] = defaultdict(set)
+        
+        for curr, prev in prerequisites:
+            indegrees[curr] += 1
+            adjacents[prev].add(curr)
+            
+        no_deps: List[int] = [i for i in range(numCourses) if not i in indegrees]
+        order: List[int] = []
+        while no_deps:
+            curr: int = no_deps.pop()
+            order.append(curr)
+            for adjacent in adjacents[curr]:
+                indegrees[adjacent] -= 1
+                if indegrees[adjacent] == 0:
+                    no_deps.append(adjacent)
+                    
+        return order if len(order) == numCourses else []
