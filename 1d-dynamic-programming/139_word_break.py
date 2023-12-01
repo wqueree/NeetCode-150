a46dfa0a8511c9f3e5c582385519191f4d517f76
Will Queree <wqueree@gmail.com>
2023-12-01 14:07:35 +0000

    Refactor 139

--- 1d-dynamic-programming/139_word_break.py
+++ 1d-dynamic-programming/139_word_break.py
@@ -6,28 +6,24 @@ class Solution:
 
 
     @cache
-    def wordBreakCache(self, s: str, wordEnd: int, wordDict: Tuple[str]):
+    def wordBreakCache(self, s: str, wordEnd: int, wordDict: Tuple[str]) -> bool:
         if wordEnd < 0:
             return True
         for word in wordDict:
             wordStart: int = wordEnd - len(word) + 1
-            if wordStart < 0:
-                continue
-            if s[wordStart : wordEnd + 1] == word and self.wordBreakCache(s, wordStart - 1, wordDict):
+            if wordStart >= 0 and s[wordStart : wordEnd + 1] == word and self.wordBreakCache(s, wordStart - 1, wordDict):
                 return True
         return False
 
 
-    def wordBreakDynamic(self, s: str, wordEnd: int, wordDict: List[str], memo: List[int]):
+    def wordBreakMemo(self, s: str, wordEnd: int, wordDict: List[str], memo: List[int]) -> bool:
         if wordEnd < 0:
             return True
         if memo[wordEnd] != -1:
             return memo[wordEnd] == 1
         for word in wordDict:
             wordStart: int = wordEnd - len(word) + 1
-            if wordStart < 0:
-                continue
-            if s[wordStart : wordEnd + 1] == word and self.wordBreakDynamic(s, wordStart - 1, wordDict, memo):
+            if wordStart >= 0 and s[wordStart : wordEnd + 1] == word and self.wordBreakMemo(s, wordStart - 1, wordDict, memo):
                 memo[wordEnd] = 1
                 return True
         memo[wordEnd] = 0
@@ -40,4 +36,4 @@ class Solution:
 
         # memoization
         memo: List[int] = [-1 for _ in range(len(s))]
-        return self.wordBreakDynamic(s, len(s) - 1, wordDict, memo)
+        return self.wordBreakMemo(s, len(s) - 1, wordDict, memo)
