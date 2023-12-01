7956c657d27363623bf8982acd633e650f2424d0
Will Queree <wqueree@gmail.com>
2023-12-01 22:57:44 +0000

    Improving 253

--- intervals/253_meeting_rooms_ii.py
+++ intervals/253_meeting_rooms_ii.py
@@ -6,13 +6,8 @@ class Solution:
 
 
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals:
-            return 0
-
         intervals.sort(key = lambda times: times[0])
-        rooms: List[int] = list()
-        heapq.heappush(rooms, intervals[0][1])
-
+        rooms: List[int] = [intervals[0][1]]
         for i in range(1, len(intervals)):
             meeting_start, meeting_end = intervals[i]
             if rooms[0] <= meeting_start:
