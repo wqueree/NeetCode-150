543244f5ee9edcd0d6ffdb334a3f8b658a10fec7
Will Queree <wqueree@gmail.com>
2023-12-01 14:19:55 +0000

    Added 207

---
+++ graphs/207_course_schedule.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from typing import Dict, List, Set
+
+
+class Solution:
+
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegrees: Dict[int, int] = defaultdict(int)
+        adjacents: Dict[int, Set[int]] = defaultdict(set)
+        
+        for curr, prev in prerequisites:
+            indegrees[curr] += 1
+            adjacents[prev].add(curr)
+            
+        no_deps: List[int] = [i for i in range(numCourses) if not i in indegrees]
+        order: List[int] = []
+        while no_deps:
+            curr = no_deps.pop()
+            order.append(curr)
+            for adjacent in adjacents[curr]:
+                indegrees[adjacent] -= 1
+                if indegrees[adjacent] == 0:
+                    no_deps.append(adjacent)
+                    
+        return len(order) == numCourses
